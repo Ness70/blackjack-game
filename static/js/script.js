7aa3211c647e2378bde704b32b30fd2a5172560a
Ness <whistler990@gmail.com>
2022-07-04 23:05:50 +0300

    Added functionality to Player and Dealers boxes
    Made new code entries for script.js, adding functionality to the hit and deal buttons that enable random cards displayed upon button click.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,46 @@
+//Blackjack Game
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'],
+    'cardsValue': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'Q': 10, 'J': 10, 'A': [1, 11]}
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+const hitSound = new Audio('static/sounds/swish.m4a');
+
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
-    alert('Ouch, You just clicked me!');
+    let card = randomCard();
+    showCard(card, YOU);
+    console.log(card);
+}
+
+function randomCard() {
+    let randomIndex = Math.floor(Math.random()*13);
+    return blackjackGame['cards'][randomIndex];
+}
+
+
+function showCard(card, activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = `static/images/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+
+    for(i=0; i<yourImages.length; i++){
+        yourImages[i].remove();
+    }
+
+    for(i=0; i<dealerImages.length; i++){
+        dealerImages[i].remove();
+    }
 }
