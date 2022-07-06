d30e372adc5fcbe6e8d4ceaee2840c5ead92b87e
Ness <whistler990@gmail.com>
2022-07-06 20:29:05 +0300

    Update script.js
    1. added score update functionality
    2. added logic to the card count (bust if exceed 21)

--- static/js/script.js
+++ static/js/script.js
@@ -11,12 +11,15 @@ const DEALER = blackjackGame['dealer']
 const hitSound = new Audio('static/sounds/swish.m4a');
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
     let card = randomCard();
     showCard(card, YOU);
     console.log(card);
+    updateScore(card, YOU);
+    showScore(YOU);
 }
 
 function randomCard() {
@@ -25,11 +28,13 @@ function randomCard() {
 }
 
 
-function showCard(card, activePlayer) {
-    let cardImage = document.createElement('img');
-    cardImage.src = `static/images/${card}.png`;
-    document.querySelector(activePlayer['div']).appendChild(cardImage);
-    hitSound.play();
+function showCard(card, activePlayer) { 
+    if (activePlayer['score'] <= 21) { //showing a bust if player exceeds 21
+        let cardImage = document.createElement('img');
+        cardImage.src = `static/images/${card}.png`;
+        document.querySelector(activePlayer['div']).appendChild(cardImage);
+        hitSound.play();
+    }
 }
 
 function blackjackDeal() {
@@ -43,10 +48,52 @@ function blackjackDeal() {
     for(i=0; i<dealerImages.length; i++){
         dealerImages[i].remove();
     }
+
+    //reset score to zero
+    YOU['score'] = 0;
+    DEALER['score'] = 0;
+
+    document.querySelector('#your-blackjack-result').textContent = 0;
+    document.querySelector('#dealer-blackjack-result').textContent = 0;
+    
+    document.querySelector('#your-blackjack-result').style.color = 'white';   
+    document.querySelector('#dealer-blackjack-result').style.color = 'white'; 
 }
 
-function updateScore(card, activePlayer){
-    activePlayer['score']
+function updateScore(card, activePlayer) {
+    //For Ace card; If adding 11 keeps me below 21, add 11 otherwise add 1
+    if (card === 'A'){
+        if(activePlayer['score'] + blackjackGame['cardsValue'][card][1] <= 21){ //statement checks is the card can increase by 11(from A card array value = 11) and still remain below 21
+            activePlayer['score'] += blackjackGame['cardsValue'][card][1]; //..then increase by 11 (A: 11)
+        } else {
+            activePlayer['score'] += blackjackGame['cardsValue'][card][0]; //..otherwise, increase by 1 (A: 1)
+        }
+    } else {
+        activePlayer['score'] += blackjackGame['cardsValue'][card]; //otherwise continue with actual key value of cards from rest of cards array
+    }
 }
 
-//minute: 6:13:00/7:57:277
+function showScore(activePlayer) {
+    if (activePlayer['score'] > 21) { //checking if the value of the total cards exceed 21, hence update the scoreSpan html to BUST!
+        document.querySelector(activePlayer['scoreSpan']).textContent = 'BUST!';
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    } else {
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+    }
+}
+
+function dealerLogic() {
+    let card = randomCard();
+    showCard(card, DEALER);
+    updateScore(card, DEALER);
+    showScore(DEALER);
+}
+
+
+let animals = ["eagle", "osprey", "salmon"];
+
+let key = animal => animal === "salmon";
+
+if (animals.some(key)) {
+    console.log("swim");
+}
